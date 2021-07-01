dbc0e67aaf260b4c9489bd8b310ba222731bfc00
Cedric Sobey <cedsobey@gmail.com>
2021-07-01 17:17:35 +0200

    adding color picker functionality

--- designs.js
+++ designs.js
@@ -1,25 +1,52 @@
+const sizePicker = document.getElementById('sizePicker');
+const colorPicker = document.getElementById("colorPicker");
 // Select color input
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 // makeGrid()
 
-// function makeGrid() {
+function makeGrid(width,height) {
+    clearGrid()
 
-// }
-
-const sizePicker = document.getElementById('sizePicker');
-const displaySize = document.getElementById('displaySize');
+    for (y = 0; y < height; y++) {
+        const oneRow = document.createElement('tr');
+        pixelCanvas.appendChild(oneRow);
+        for (x = 0; x < width; x++) {
+            const onePixel = document.createElement('td');
+            const pixelCanvas = document.querySelector('#pixelCanvas');
+            oneRow.appendChild(onePixel);
+            //create unique ID for each pixel
+            uniquieId = (y+"-"+x);
+            onePixel.setAttribute('id', uniquieId)
+        }
+    }
+    
+ }
 
 function submitSize(event) {
     event.preventDefault();
+    var height = document.getElementById("inputHeight").value;
+    var width = document.getElementById("inputWidth").value;
+    makeGrid(width,height);
+}
 
-    var y = document.getElementById("inputHeight").value;
-    var x = document.getElementById("inputWidth").value;
+function clearGrid() {
+    const pixelCanvas = document.querySelector('#pixelCanvas');
+    const pixelCanvasRows = pixelCanvas.childElementCount
+    if (pixelCanvasRows > 0) {
+        for (y = 0; y < pixelCanvasRows; y++) {
 
-    displaySize.textContent = `Canvas size = ${y} high x ${x} wide`;
+            pixelCanvas.firstElementChild.remove();
+        }
+    }
 }
 
 
+function setPixelColor() {
+    console.log(colorPicker.value);
+}
+
 
 sizePicker.addEventListener('submit', submitSize);
+colorPicker.addEventListener('input', setPixelColor);
