24ec92d0623170cb12edc7a5fe9715ec08ea2a8a
Cedric Sobey <cedsobey@gmail.com>
2021-07-02 12:14:50 +0200

    correct variables, formatting and syntax

--- designs.js
+++ designs.js
@@ -1,26 +1,26 @@
 const sizePicker = document.getElementById('sizePicker');
+var uniquieId = '';
 
 function makeGrid(width,height) {
     //Clear Grid & Event listener       
         const pixelCanvas = document.querySelector('#pixelCanvas');
-        const pixelCanvasRows = pixelCanvas.childElementCount
+        const pixelCanvasRows = pixelCanvas.childElementCount;
         if (pixelCanvasRows > 0) {
-            for (y = 0; y < pixelCanvasRows; y++) {
+            for (let y = 0; y < pixelCanvasRows; y++) {
                 pixelCanvas.firstElementChild.remove();
             }
         }
         document.removeEventListener('click', pixelCanvasListening);
     //Create grid
-    for (y = 0; y < height; y++) {
+    for (let y = 0; y < height; y++) {
         const oneRow = document.createElement('tr');
         pixelCanvas.appendChild(oneRow);
-        for (x = 0; x < width; x++) {
+        for (let x = 0; x < width; x++) {
             const onePixel = document.createElement('td');
-            const pixelCanvas = document.querySelector('#pixelCanvas');
             oneRow.appendChild(onePixel);
             //create unique ID for each pixel
-            const uniquieId = (y+"-"+x);
-            onePixel.setAttribute('id', uniquieId)
+            uniquieId = (y+"-"+x);
+            onePixel.setAttribute('id', uniquieId);
         }
     }
     pixelCanvas.addEventListener('mousedown', pixelCanvasListening);
@@ -28,27 +28,24 @@ function makeGrid(width,height) {
 
 // Listening event for pixel Canvas
 function pixelCanvasListening (event) {
-    // console.log('Pixel: ' + event.target.id);
-    uniquieId = event.target.id
-    addColorByClick(uniquieId)
+    uniquieId = event.target.id;
+    addColorByClick(uniquieId);
 }
 
 // Apply color to clicked pixel
 function addColorByClick() {
     if (uniquieId !== "pixelCanvas") {
         const colorPicker = document.getElementById("colorPicker").value;
-        clickedPixel = document.getElementById(uniquieId);
+        let clickedPixel = document.getElementById(uniquieId);
         clickedPixel.style.backgroundColor = colorPicker;
         console.log(document.getElementById("pixelCanvas").id);
     }
-    
-    
-    }
+}
 
 function submitSize(event) {
     event.preventDefault();
-    var height = document.getElementById("inputHeight").value;
-    var width = document.getElementById("inputWidth").value;
+    let height = document.getElementById("inputHeight").value;
+    let width = document.getElementById("inputWidth").value;
     makeGrid(width,height);
 }
 
